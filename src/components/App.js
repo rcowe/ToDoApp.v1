046b575dcbd30e28b99f75e9c646dd27e4861cf8
rcbdelarosa <rcb.delarosa@gmail.com>
2021-02-04 02:18:38 -0600

    second commit bulding app and form files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import Form from './Form'
 
 export default function App(props) {
 	const [todo, setTodo] = useState('');
 
 
 
-	return <div className="add-here"></div>;
+	return <div className="app-wrapper">
+		<header>
+			<h1>
+				Rosa's To Do App
+			</h1>
+		</header>
+		<Form />
+	</div>;
 }
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Form(props) {
+
+	return (
+	<form>
+		<input type="text" className="input-line"/>
+		<button className="input-submit" type="submit">+</button>
+	</form>
+	);
+}
